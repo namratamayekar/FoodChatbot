baebca8f1ca379ee090ac40005d63be04ff0ebf8
nams1710 <nams1710@gmail.com>
2024-09-19 18:40:13 +0200

    edited add_to_order function

--- generic_helper.py
+++ generic_helper.py
@@ -4,7 +4,14 @@ import re
 def extract_session_id(session_str: str):
     match = re.search(r"/sessions/(.*?)/contexts/", session_str)
     if match:
-        extracted_string = match.group(0)
+        extracted_string = match.group(1)
         return extracted_string
 
     return ""
+
+def get_str_from_food_dict(food_dict: dict):
+    result = ", ".join([f"{int(value)} {key}" for key, value in food_dict.items()])
+    return result
+
+if __name__ == "__main__":
+    print(get_str_from_food_dict({"samosa":2, "chole":1}))
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi import Request
 from fastapi.responses import JSONResponse
 import db_helper
+import generic_helper
 
 app = FastAPI()
 
@@ -19,6 +20,7 @@ async def handle_request(request: Request):
     intent = payload['queryResult']['intent']['displayName']
     parameters = payload['queryResult']['parameters']
     output_contexts = payload['queryResult']['outputContexts']
+    session_id = generic_helper.extract_session_id(output_contexts[0]["name"])
 
     intent_handler_dict = {
         'order.add - context: ongoing-order': add_to_order,
@@ -26,16 +28,26 @@ async def handle_request(request: Request):
         #'order.complete - context: ongoing-order': complete_order,
         'track.order - context: ongoing-tracking': track_order
     }
-    return intent_handler_dict[intent](parameters)
+    return intent_handler_dict[intent](parameters, session_id)
 
-def add_to_order(parameters: dict):
+def add_to_order(parameters: dict, session_id: str):
     food_items = parameters["food-item"]
     quantities = parameters["number"]
 
     if len(food_items) != len(quantities):
         fulfillment_text = "Sorry I didn't understand. Can you please specify food items and quantities clearly?"
     else:
-        fulfillment_text = f"Recieved {food_items} and {quantities} in the backend"
+        new_food_dict = dict(zip(food_items, quantities))
+
+        if session_id in inprogress_orders:
+            current_food_dict = inprogress_orders[session_id]
+            current_food_dict.update(new_food_dict)
+            inprogress_orders[session_id] = current_food_dict
+        else:
+            inprogress_orders[session_id] = new_food_dict
+
+        order_str = generic_helper.get_str_from_food_dict(inprogress_orders[session_id])
+        fulfillment_text = f"So far you have: {order_str}. Do you need anything else?"
 
     return JSONResponse(content={
         "fulfillmentText": fulfillment_text
