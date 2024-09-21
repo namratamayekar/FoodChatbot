22afec837e25a422d75ac9ef46149fefc002a5a6
nams1710 <nams1710@gmail.com>
2024-09-21 11:16:14 +0200

    Added insert_order_item to db_helper

--- db_helper.py
+++ db_helper.py
@@ -8,6 +8,39 @@ cnx = mysql.connector.connect(
     database="pandeyji_eatery"
 )
 
+# Function to call the MySQL stored procedure and insert an order item
+def insert_order_item(food_item, quantity, order_id):
+    try:
+        cursor = cnx.cursor()
+
+        # Calling the stored procedure
+        cursor.callproc('insert_order_item', (food_item, quantity, order_id))
+
+        # Committing the changes
+        cnx.commit()
+
+        # Closing the cursor
+        cursor.close()
+
+        print("Order item inserted successfully!")
+
+        return 1
+
+    except mysql.connector.Error as err:
+        print(f"Error inserting order item: {err}")
+
+        # Rollback changes if necessary
+        cnx.rollback()
+
+        return -1
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        # Rollback changes if necessary
+        cnx.rollback()
+
+        return -1
+
 # Function to get the next available order_id
 def get_next_order_id():
     cursor = cnx.cursor()
